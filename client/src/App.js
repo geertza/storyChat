c10aff290b57a4d8764c919d6ad3264eb83cbbe3
geertza <geertza@gmail.com>
2020-07-27 20:59:02 -0600

    login.js added working on joining servers

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './components/Home';
-import About from './components/About';
-import logo from "./logo.svg";
+
 import "./App.css";
-
+import login from './components/login'
 function App() {
-  const [count, setCount] = useState(0);
-
+  
+  
+  
+  
   return (
     <Router>
 
-      <div className="App">
-        <div style={{ backgroundColor: '#000', color: '#eee' }}>
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Welcome to React</h2>
-        </div>
-        <button onClick={() => setCount(c => ++c)}>
-          Count: {count}
-        </button>
+        <div>
 
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
+          <Route exact path="/" component={login} />
+          
+         
         </Switch>
       </div>
     </Router>
